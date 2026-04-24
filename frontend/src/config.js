9ab7ed363ea341e0058f8440c822b6ba37b84d36
Patricia844 <mukunzapatricia@gmail.com>
2026-04-24 12:27:03 +0200

    Fix production API URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,11 @@
-// This file helps switch between development and production
+﻿// This file helps switch between development and production
 
 const config = {
     development: {
         apiUrl: 'http://localhost:5000/api'
     },
     production: {
-        apiUrl: 'https://pigfarm-backend-3fsv.onrender.com'  // Replace with your Render URL
+        apiUrl: 'https://pigfarm-backend-3fsv.onrender.com/api'  // Replace with your Render URL
     }
 };
 
